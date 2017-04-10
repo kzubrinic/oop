48dcacdfe4fa1ed2e8d3f913558702f5196020d9
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2017-04-10 20:56:52 +0200

    modified:   README.TXT 	modified:   Tokovi/Primjer7.java 	modified:   Tokovi/Primjer8.java 	modified:   bluej.pkg 	modified:   package.bluej

--- primjeri/v06/Tokovi/Primjer7.java
+++ primjeri/v06/Tokovi/Primjer7.java
@@ -7,14 +7,39 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.util.Scanner;
+import java.nio.charset.Charset;
 
 public class Primjer7 {
 
     public void citajRetke() {
         String c = "";
         String ime = "README.TXT";
+        // podrazumijevana kodna stranica - UTF-8
         try(BufferedReader dat = new BufferedReader(new FileReader(ime))) {
+            while ((c = dat.readLine()) != null) {
+                // readLine() skida znak za novi red pa ga treba dodati za prikaz na ekranu
+                System.out.println(c);
+            }
+        } catch (IOException e) {
+            System.out.println("Greška èitanja datoteke " + ime + " !");
+            e.printStackTrace();
+        }
+    }
+    
+    public void citajRetke2() {
+        String c = "";
+        String ime = "README.TXT";
+        // izabrana kodna stranica - windows-1250
+        Charset ks = Charset.forName("windows-1250");
+        try(BufferedReader dat = new BufferedReader(
+            new InputStreamReader(
+                new BufferedInputStream(
+                    new FileInputStream(ime)), ks))){
+
             while ((c = dat.readLine()) != null) {
                 // readLine() skida znak za novi red pa ga treba dodati za prikaz na ekranu
                 System.out.println(c);
@@ -29,6 +54,26 @@ public class Primjer7 {
         String c = "";
         String ime = "README.TXT";
         try(Scanner s = new Scanner(new BufferedReader(new FileReader(ime)))) {
+            s.useDelimiter("o"); // razbijamo na slovu 'o'
+            while ((s.hasNext())) {
+                // metoda hasNext() provjerava da li postoji sljedeæi token
+                // metoda next() pristupa nizu znakova
+                System.out.println(s.next());
+            }
+        } catch (IOException e) {
+            System.out.println("Greška èitanja datoteke " + ime + " !");
+            e.printStackTrace();
+        }
+    }
+    
+    public void citajTokene2() {
+        String c = "";
+        String ime = "README.TXT";
+        Charset ks = Charset.forName("windows-1250");
+        try(Scanner s = new Scanner(new BufferedReader(
+            new InputStreamReader(
+                new BufferedInputStream(
+                    new FileInputStream(ime)), ks)))) {
             s.useDelimiter("o"); // razbijamo na slovu 'o'
             while ((s.hasNext())) {
                 // metoda hasNext() provjerava da li postoji sljedeæi token
--- primjeri/v06/Tokovi/Primjer8.java
+++ primjeri/v06/Tokovi/Primjer8.java
@@ -12,10 +12,89 @@ import java.io.FileReader;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.FileWriter;
-
+import java.nio.charset.Charset;
+import java.io.OutputStreamWriter;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 public class Primjer8 {
 
     public void unosPodataka() {
+        File d = stvaranjeDatoteke();
+            // drugi parametar konstruktora FileWriter true znaèi da se podaci dodaju
+            //  u datoteku (slièno kao append u C-u)
+            // Inaèe je "normalno" ponašanje da se stara datioteka gazi novom!
+        try(PrintWriter datoteka = new PrintWriter(new FileWriter(d,true))) {
+            // unos podataka
+            datoteka.println("Ovo ide u prvi red");
+            datoteka.println("Ovo ide u drugi red "+175);
+            datoteka.println("Primjeri naših èestih æukastih žalosnih ðonovskih slova");
+            datoteka.print("Ovo ide u posljednji red "+3.76);
+            datoteka.close();
+            prikazi(d);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Greška rada s datotekom");
+            e.printStackTrace();
+        }
+
+        
+    }
+    
+    public void unosPodataka2() {
+        File d = stvaranjeDatoteke();
+        Charset ks = Charset.forName("windows-1250");
+            // drugi parametar konstruktora FileWriter true znaèi da se podaci dodaju
+            //  u datoteku (slièno kao append u C-u)
+            // Inaèe je "normalno" ponašanje da se stara datioteka gazi novom!
+        try(PrintWriter datoteka = new PrintWriter(
+            new OutputStreamWriter(
+                new BufferedOutputStream(
+                    new FileOutputStream(d)),ks))) {
+            // unos podataka
+            datoteka.println("Ovo ide u prvi red");
+            datoteka.println("Ovo ide u drugi red "+175);
+            datoteka.println("Primjeri naših èestih æukastih žalosnih ðonovskih slova");
+            datoteka.print("Ovo ide u posljednji red "+3.76);
+            datoteka.close();
+            prikazi(d,ks);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Greška rada s datotekom");
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+    private void prikazi(File d) throws IOException{
+        String c = "";
+        try(BufferedReader dat = new BufferedReader(new FileReader(d))){
+          while ((c = dat.readLine()) != null) {
+            System.out.println(c);
+          }
+        }
+    }
+    
+    private void prikazi(File d, Charset ks) throws IOException{
+        String c = "";
+        try(BufferedReader dat = new BufferedReader(
+            new InputStreamReader(
+                new BufferedInputStream(
+                    new FileInputStream(d)), ks))){
+          while ((c = dat.readLine()) != null) {
+            System.out.println(c);
+          }
+        }
+    }
+    
+    private File stvaranjeDatoteke() {
         Unos u = new Unos();
         String dat=u.unesiString("Unesi naziv datoteke: ");
         File d = new File(dat);
@@ -27,33 +106,6 @@ public class Primjer8 {
                 e.printStackTrace();
             }
         }
-            // drugi parametar konstruktora FileWriter true znaèi da se podaci dodaju
-            //  u datoteku (slièno kao append u C-u)
-            // Inaèe je "normalno" ponašanje da se stara datioteka gazi novom!
-        try(PrintWriter datoteka = new PrintWriter(new FileWriter(dat,true))) {
-            // unos podataka
-            datoteka.println("Ovo ide u prvi red");
-            datoteka.println("Ovo ide u drugi red "+175);
-            datoteka.println("Primjeri naših èestih æukastih žalosnih ðonovskih slova");
-            datoteka.print("Ovo ide u posljednji red "+3.76);
-            datoteka.close();
-            prikazi(dat);
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("Greška rada s datotekom");
-            e.printStackTrace();
-        }
-
-        
-    }
-    private void prikazi(String ime) throws IOException{
-        String c = "";
-        try(BufferedReader dat = new BufferedReader(new FileReader(ime))){
-          while ((c = dat.readLine()) != null) {
-            System.out.println(c);
-          }
-        }
+        return d;
     }
 }
