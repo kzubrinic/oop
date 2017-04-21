24bf61179b98bc3c9246f5727efe1d4b9cbc6869
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2017-04-21 14:08:11 +0200

    Dodani primjeri za rad s relacijskim bazama podataka

---
+++ primjeri/v08/upit.sql
@@ -0,0 +1,3 @@
+CREATE TABLE KNJIGE (ID INT PRIMARY KEY,  NAZIV VARCHAR(30), AUTOR VARCHAR (255));
+
+INSERT INTO KNJIGE (ID, NAZIV, AUTOR) VALUES (101, 'Head First Java', 'Kathy Sierra'),(102, 'JAVA in 8 Hours', 'Ray Yao'),(103, 'Java: A Beginner''s Guide', 'Herbert Schildt'),(104, 'Effective Java', 'Joshua Block');
