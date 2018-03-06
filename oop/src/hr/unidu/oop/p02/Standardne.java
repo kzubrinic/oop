28bd3994abf5771f12ef4a7d0acaf24d49f7086e
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2018-03-06 09:45:44 +0100

    Standardne metode

--- oop/src/hr/unidu/oop/p02/Standardne.java
+++ oop/src/hr/unidu/oop/p02/Standardne.java
@@ -1,5 +1,8 @@
 package hr.unidu.oop.p02;
 import java.util.Date;
+
+import hr.unidu.oop.p01.Osoba;
+
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -11,6 +14,8 @@ public class Standardne
 {
     public void odrediDatum()
     {
+    	System.out.println("");
+    	System.out.println("---- METODA odrediDatum ---- ");
         LocalDateTime datVri = LocalDateTime.now();
         LocalDate danas = datVri.toLocalDate();
         LocalTime sada = datVri.toLocalTime();
@@ -39,24 +44,37 @@ public class Standardne
     }
     public void desetSlucajnih(int pocetak, int kraj)
     {
+    	System.out.println("");
+    	System.out.println("---- METODA desetSlucajnih ---- ");
         for (int i = 0; i < 10; i++)
         System.out.println(pocetak +(int)(Math.random()*kraj));
     }
     public void josNekeMath()
     {
+    	System.out.println("");
+    	System.out.println("---- METODA josNekeMath ---- ");
         double x = 10, y = 7.671, neg = -12; 
         System.out.println("Modulo broja "+ neg + " je " + Math.abs(neg));
-        System.out.println("Od dva broja "+ x + " i " + y + ", ve�i je " + Math.max(x,y));        
-        System.out.println(x + " na tre�u je " + Math.pow(x, 3));
-        System.out.println(y + " zaokru�en iznosi " + Math.round(y));        
+        System.out.println("Od dva broja "+ x + " i " + y + ", veći je " + Math.max(x,y));        
+        System.out.println(x + " na treću je " + Math.pow(x, 3));
+        System.out.println(y + " zaokružen iznosi " + Math.round(y));        
         System.out.println("Kvadratni korijen broja "+ x + " je " + Math.sqrt(x));
     }
     public void formatiraj()
     {
+    	System.out.println("");
+    	System.out.println("---- METODA formatiraj ---- ");
         float a = 2.7189f, b = 1.122f;
         String novi = String.format("%.2f %.1f %.3f", a, b, Math.PI);
         System.out.println(novi);
 
+    }
+    public static void main(String[] args){
+        Standardne s = new Standardne();
+        s.odrediDatum();
+        s.desetSlucajnih(10, 100);
+        s.josNekeMath();
+        s.formatiraj();
     }
     
 }
