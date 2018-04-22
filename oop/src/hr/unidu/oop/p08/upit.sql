79878bc1df89d0196475569d7ea2d225adedb4ed
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2018-04-22 16:50:06 +0200

    Izmjena Testbaza derby->sqlite

--- oop/src/hr/unidu/oop/p08/upit.sql
+++ oop/src/hr/unidu/oop/p08/upit.sql
@@ -1,3 +1,3 @@
-CREATE TABLE KNJIGE (ID INT PRIMARY KEY,  NAZIV VARCHAR(30), AUTOR VARCHAR (255));
+CREATE TABLE KNJIGE (ID INTEGER PRIMARY KEY,  NAZIV TEXT, AUTOR TEXT);
 
 INSERT INTO KNJIGE (ID, NAZIV, AUTOR) VALUES (101, 'Head First Java', 'Kathy Sierra'),(102, 'JAVA in 8 Hours', 'Ray Yao'),(103, 'Java: A Beginner''s Guide', 'Herbert Schildt'),(104, 'Effective Java', 'Joshua Block');
