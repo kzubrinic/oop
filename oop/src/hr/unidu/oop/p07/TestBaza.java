458bffd4003e6bc1642a650fb6f0cfb86331a309
Kruno <kruno.zubrinic@gmail.com>
2020-04-09 11:21:23 +0200

    Ispravak u metodi obrada

--- oop/src/hr/unidu/oop/p07/TestBaza.java
+++ oop/src/hr/unidu/oop/p07/TestBaza.java
@@ -173,13 +173,19 @@ public class TestBaza	{
 			unos(new Knjiga(104, "Effective Java", "Joshua Block"));
 			// čita sve iz tablice knjige i prikazuje na zaslonu. 
 			// sada se prikazuje i novo dodani zapis s id-jem 200
-			citanjeSvih();
+			l = citanjeSvih();
+			if(l!=null) {
+				System.out.println("DOHVAĆENE KNJIGE NAKON INSERTA:");
+				for(Knjiga k : l)
+					System.out.println(k);
+				System.out.println("---------------------------------");
+			}
 			// Mijenja se naziv knjige s id-jem 103
 			izmjena(new Knjiga(102, "JAVA in 8 Hours 2nd updated ed", "Ray Yao"));
 			// čita sve iz tablice knjige i prikazuje na zaslonu.
 			l = citanjeSvih();
 			if(l!=null) {
-				System.out.println("DOHVAĆENE KNJIGE:");
+				System.out.println("DOHVAĆENE KNJIGE NAKON IZMJENA:");
 				for(Knjiga k : l)
 					System.out.println(k);
 				System.out.println("---------------------------------");
