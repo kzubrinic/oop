4ab66decf51d96cb5ba84bc29698fd17b06dba00
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2020-04-17 13:20:55 +0200

    Dorađen primjer inicijalizacije liste

--- oop/src/hr/unidu/oop/p05/PrimjerInicijalizacijeKolekcije.java
+++ oop/src/hr/unidu/oop/p05/PrimjerInicijalizacijeKolekcije.java
@@ -1,10 +1,12 @@
 package hr.unidu.oop.p05;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PrimjerInicijalizacijeKolekcije {
-	private List<String> l = new ArrayList<>(){ // anonimna unutarnja klasa tipa ArrayList<String>
+	@SuppressWarnings("serial")
+	private List<String> l1 = new ArrayList<>(){ // anonimna unutarnja klasa tipa ArrayList<String>
         { // inicijalizator instance objekta 
             add("prvi"); 
             add("drugi"); 
@@ -12,11 +14,14 @@ public class PrimjerInicijalizacijeKolekcije {
             add("četvrti");
         } 
     }; 
+    // Inicijalizacija korištenjem metode asList() klase Arrays
+    private List<String> l2 = Arrays.asList("prvi", "drugi" ,"treći", "četvrti");
+
 	public static void main(String[] args) {
 		PrimjerInicijalizacijeKolekcije p = new PrimjerInicijalizacijeKolekcije();
-		for(String s: p.l)
+		for(String s: p.l1)
 			System.out.println(s);
-
+		for(String s: p.l2)
+			System.out.println(s);
 	}
-
 }
