a793d4a89f9d64b4daa18e25399c643e1c4ab0a4
Kruno Zubrinic <kruno.zubrinic@gmail.com>
2021-05-05 21:23:08 +0200

    Ažuriran punjač slika

--- oop/src/hr/unidu/oop/p10/PunjacSlika.java
+++ oop/src/hr/unidu/oop/p10/PunjacSlika.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PunjacSlika {
-    public static final String OSOBA1 = "oop/slike/mara.jpg";
-    public static final String OSOBA2 = "oop/slike/iva.jpg";
-    public static final String OSOBA3 = "oop/slike/vedrana.jpg";
+    public static final String OSOBA1 = "slike/mara.jpg";
+    public static final String OSOBA2 = "slike/iva.jpg";
+    public static final String OSOBA3 = "slike/vedrana.jpg";
 	public static List<Osoba> napuniListuOsoba(){
 		List<Osoba> st = new ArrayList<>();
-		String PERO = "oop/slike/pero.jpg";
+		String PERO = "slike/pero.jpg";
 		st.add(new Osoba("Pero", "Perić", PERO, 49));
-		String ANA = "oop/slike/ana.jpg";
+		String ANA = "slike/ana.jpg";
 		st.add(new Osoba("Ana", "Anić", ANA, 19));
-		String BERO = "oop/slike/bero.jpg";
+		String BERO = "slike/bero.jpg";
 		st.add(new Osoba("Bero", "Berić", BERO, 37));
-		String VLAHO = "oop/slike/vlaho.jpg";
+		String VLAHO = "slike/vlaho.jpg";
 		st.add(new Osoba("Vlaho", "Vlahić", VLAHO, 72));
-		String IVO = "oop/slike/ivo.jpg";
+		String IVO = "slike/ivo.jpg";
 		st.add(new Osoba("Ivo", "Ivić", IVO, 18));
 		return st;
 	}
