62265d502908fc462846fbc5ca31c3cc4e026529
Kruno Zubrinic <kruno.zubrinic@gmail.com>
2021-05-05 20:34:07 +0200

    Ažuriran primjer rada s "bazenom dretvi"

--- oop/src/hr/unidu/oop/p09/bazendretvi/PrimjerBazenaDretvi.java
+++ oop/src/hr/unidu/oop/p09/bazendretvi/PrimjerBazenaDretvi.java
@@ -1,7 +1,11 @@
 package hr.unidu.oop.p09.bazendretvi;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 
 public class PrimjerBazenaDretvi {
@@ -11,15 +15,25 @@ public class PrimjerBazenaDretvi {
         ExecutorService ex1 = Executors.newFixedThreadPool(5);
         // "Bazen dretvi" dinamičke veličine. Kada je potrebno, nova dretva se
         //  stvara. Ako je obrada neke od prethodno stvorenih završila, ta
-        //  dretva se "reciklira".
+        //  dretva se "reciklira". 
 		ExecutorService ex2 = Executors.newCachedThreadPool();
         for (int i = 0; i < 10; i++) {
             Runnable radna = new RadnaDretva(i+1);
+            // U prvi bazen spremamo dretve koje ne vraćaju povratnu vrijednost
             ex1.execute(radna);
         }
 		for (int i = 0; i < 10; i++) {
-            Runnable radna = new RadnaDretva(i+1);
-            ex2.execute(radna);
+			// U drugi bazen spremamo dretve koje vraćaju povratnu vrijednost
+			Callable<String> radna = new RadnaDretva(i+1); 
+			// Za vraćanje vrijednosti se koristi objekt "omotač" tipa Future 
+			Future<String> obrada = ex2.submit(radna);
+			try {
+				// Dohvaćamo vraćen u vrijednost
+				String rez = obrada.get();
+				System.out.println(rez);
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
         }
 
         ex1.shutdown();
--- oop/src/hr/unidu/oop/p09/bazendretvi/RadnaDretva.java
+++ oop/src/hr/unidu/oop/p09/bazendretvi/RadnaDretva.java
@@ -1,17 +1,30 @@
 package hr.unidu.oop.p09.bazendretvi;
 
-public class RadnaDretva implements Runnable {
+import java.util.concurrent.Callable;
+
+// Klasa implementira oba sučelja za rad s dretvama
+public class RadnaDretva implements Runnable, Callable<String> {
     private int nazivDretve;
     public RadnaDretva(int s){
         nazivDretve=s;
     }
  
+    // Obrada kada se ne vraća nikaka vrijednost 
     public void run() {
         System.out.println(Thread.currentThread().getName()+" (Start) dretva = "+nazivDretve);
         obrada();
         System.out.println(Thread.currentThread().getName()+" (End)");
     }
  
+    // Obrada kada se vraća podatak tipa string
+	@Override
+	public String call() throws Exception {
+		System.out.println(Thread.currentThread().getName()+" (Start) dretva = "+nazivDretve);
+        obrada();
+        System.out.println(Thread.currentThread().getName()+" (End)");
+		return "Vraća: " + Thread.currentThread().getName() + " " + nazivDretve;
+	}
+	
     private void obrada() {
     	try {
     		System.out.println(Thread.currentThread().getName() + " (Radi) dretva = "+nazivDretve);
@@ -21,5 +34,7 @@ public class RadnaDretva implements Runnable {
 			e.printStackTrace(); 
 		}
     }
+
+
 }
 
